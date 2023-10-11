1da7528db45c1600295736614ef56a200f14cc84
Eckstein <zac@zaceckstein.com>
2023-10-10 18:20:05 -0700

    cUrl fix?

--- includes/idemailwiz-google-sheets-api.php
+++ includes/idemailwiz-google-sheets-api.php
@@ -19,6 +19,8 @@ function sync_ga_campaign_revenue_data()
     $response = idwiz_google_sheet_api_curl_call($url);
     $ga_data = json_decode($response, true);
 
+    error_log(print_r($ga_data, true));
+
     if (is_string($ga_data)) {
         return $ga_data; 
     }
@@ -116,9 +118,8 @@ function idwiz_google_sheet_api_curl_call($url)
     $ch = curl_init();
 
     // Set cURL options
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    // TODO: Add SSL verification here when on live server
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt(
