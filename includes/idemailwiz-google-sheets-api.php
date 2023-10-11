0f96dda4a279096a1dedc9485aca4c9a15fab09c
Eckstein <zac@zaceckstein.com>
2023-10-10 18:21:27 -0700

    na

--- includes/idemailwiz-google-sheets-api.php
+++ includes/idemailwiz-google-sheets-api.php
@@ -118,8 +118,8 @@ function idwiz_google_sheet_api_curl_call($url)
     $ch = curl_init();
 
     // Set cURL options
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt(
