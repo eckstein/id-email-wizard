7df8683734ccb3b3868b81ecc8c4a5332548d10e
Eckstein <zac@zaceckstein.com>
2023-11-09 09:02:18 -0800

    Image MSO 10 fix.

--- templates/chunks/css.php
+++ templates/chunks/css.php
@@ -26,6 +26,16 @@ $templateBgColor = $templateStyles['template_bg_color'] ?? '#F4F4F4';
       }
     </style>
     <![endif]-->
+<!--[if mso]>
+<style type="text/css">
+  img {
+    width: 100%!important;
+    height: auto!important;
+    display: block!important;
+  }
+</style>
+<![endif]-->
+
 <!-- Our main styles, which will be inlined by Iterable upon send -->
 <!-- Style block is wrapped in CDATA, which tells the code interpretor that it should not try to interpret the data enclosed in the tags (because it's a different language)-->
 <style type="text/css">
