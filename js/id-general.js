b62f3d5842f5ee24d5a494272e0cb925bbca9ff1
Eckstein <zac@zaceckstein.com>
2023-11-19 17:04:18 -0800

    Turn of highlighting on sync log

--- js/id-general.js
+++ js/id-general.js
@@ -552,7 +552,7 @@ if (jQuery("#syncLogContent code").length) {
 	// Auto-refresh a log element
 	let refreshWizLog = setInterval(() => {
 		jQuery("#syncLogContent code").load(idAjax.plugin_url + "/wiz-log.log", function () {
-			hljs.highlightAll();
+			//hljs.highlightAll();
 		});
 	}, 3000);
 }
