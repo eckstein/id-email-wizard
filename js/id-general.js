51a45c068edb267e8cc0c976ecf225667b13b404
Eckstein <zac@zaceckstein.com>
2023-11-19 16:58:32 -0800

    highlight refresh adjustment

--- js/id-general.js
+++ js/id-general.js
@@ -552,7 +552,7 @@ if (jQuery("#syncLogContent code").length) {
 	// Auto-refresh a log element
 	let refreshWizLog = setInterval(() => {
 		jQuery("#syncLogContent code").load(idAjax.plugin_url + "/wiz-log.log", function () {
-			hljs.highlightElement(document.querySelector("#syncLogContent code"));
+			hljs.highlightAll();
 		});
 	}, 3000);
 }
