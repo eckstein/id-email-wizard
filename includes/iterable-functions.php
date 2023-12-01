fefe1d15bedaf64abebfdd41d257ef28ea94fc1c
Eckstein <zac@zaceckstein.com>
2023-12-01 13:38:37 -0800

    Fix template sync

--- includes/iterable-functions.php
+++ includes/iterable-functions.php
@@ -47,7 +47,6 @@ function idemailwiz_get_template_data_for_iterable()
 		}
 	}
 
-	$response = [];
 	$response['alreadySent'] = false;
 	if (empty($missing)) {
 		$templateFields = array_merge($reqTemplateFields, $templateFields);
@@ -62,10 +61,10 @@ function idemailwiz_get_template_data_for_iterable()
 		if ($templateId) {
 			// Get wiz campaign based on templateId
 			$wizTemplate = get_idwiz_template($templateId);
-			$wizCampaign = get_idwiz_campaign($wizTemplate['campaignId']);
-
-			error_log(print_r($wizCampaign, true));
-
+			if ($wizTemplate) {
+				$wizCampaign = get_idwiz_campaign($wizTemplate['campaignId']);
+			}
+			
 			if ($wizCampaign && $wizCampaign['campaignState'] === 'Finished') {
 				$response['alreadySent'] = true;
 			}
