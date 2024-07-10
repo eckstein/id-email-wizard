269682635f161c85ce096d0a3241b06c378efe5e
Eckstein <zac@zaceckstein.com>
2024-07-10 13:54:01 -0700

    asdf

--- templates/reports.php
+++ templates/reports.php
@@ -64,53 +64,53 @@ $reportType = $_GET['reportType'] ?? 'home';
                         <?php
                         if ($reportType != 'home') {
                             include plugin_dir_path(__FILE__) . 'parts/dashboard-date-pickers.php';
-                        
-                        ?>
-                        <div class="wizcampaign-sections-row">
 
-                            <div class="wizcampaign-section">
-                                <h3>Narrow by cohort</h3>
-                                <?php
-                                $cohortLabels = get_all_cohort_labels();
-                                $selectedCohorts = isset($_GET['cohorts']) ? explode(',', $_GET['cohorts']) : [];
-                                ?>
-                                <select id="wiz-report-cohort-select" class="cohort-select" multiple>
-                                    <option value="all">All</option>
+                        ?>
+                            <div class="wizcampaign-sections-row">
+
+                                <div class="wizcampaign-section">
+                                    <h3>Narrow by cohort</h3>
                                     <?php
-                                    foreach ($cohortLabels as $cohortLabel) {
-                                        $cohortSelected = in_array($cohortLabel, $selectedCohorts) ? 'selected' : '';
-                                        echo '<option value="' . htmlspecialchars($cohortLabel) . '" ' . $cohortSelected . '>' . htmlspecialchars($cohortLabel) . '</option>';
-                                    }
+                                    $cohortLabels = get_all_cohort_labels();
+                                    $selectedCohorts = isset($_GET['cohorts']) ? explode(',', $_GET['cohorts']) : [];
                                     ?>
-                                </select>
-                            </div>
-                            <div class="wizcampaign-section">
-                                <h3>Sends size control</h3>
-                                <div class="wizcampaign-sections-row noWrap" id="wiz-report-sendsize-controls">
-                                    <div class="wizcampaign-section noPad">
-                                        <?php $setMin = isset($_GET['minSendSize']) ? $_GET['minSendSize'] : 1; ?>
-                                        <label for="wiz-report-sendsize-min">Min sends</label><input type="number" min="1" step="1" value="<?php echo $setMin; ?>" id="wiz-report-sendsize-min" />
-                                    </div>
-                                    <div class="wizcampaign-section noPad">
-                                        <?php $setMax = isset($_GET['maxSendSize']) ? $_GET['maxSendSize'] : 500000; ?>
-                                        <label for="wiz-report-sendsize-max">Max sends</label><input type="number" min="1" step="1" value="<?php echo $setMax; ?>" id="wiz-report-sendsize-max" />
+                                    <select id="wiz-report-cohort-select" class="cohort-select" multiple>
+                                        <option value="all">All</option>
+                                        <?php
+                                        foreach ($cohortLabels as $cohortLabel) {
+                                            $cohortSelected = in_array($cohortLabel, $selectedCohorts) ? 'selected' : '';
+                                            echo '<option value="' . htmlspecialchars($cohortLabel) . '" ' . $cohortSelected . '>' . htmlspecialchars($cohortLabel) . '</option>';
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="wizcampaign-section">
+                                    <h3>Sends size control</h3>
+                                    <div class="wizcampaign-sections-row noWrap" id="wiz-report-sendsize-controls">
+                                        <div class="wizcampaign-section noPad">
+                                            <?php $setMin = isset($_GET['minSendSize']) ? $_GET['minSendSize'] : 1; ?>
+                                            <label for="wiz-report-sendsize-min">Min sends</label><input type="number" min="1" step="1" value="<?php echo $setMin; ?>" id="wiz-report-sendsize-min" />
+                                        </div>
+                                        <div class="wizcampaign-section noPad">
+                                            <?php $setMax = isset($_GET['maxSendSize']) ? $_GET['maxSendSize'] : 500000; ?>
+                                            <label for="wiz-report-sendsize-max">Max sends</label><input type="number" min="1" step="1" value="<?php echo $setMax; ?>" id="wiz-report-sendsize-max" />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
                     </div>
-                    <?php
+                <?php
                         }
-                    if ($reportType == 'home') {
-                        include plugin_dir_path(__FILE__) . 'parts/reports/reports-home.php';
-                    } else if ($reportType == 'open-click-trends') {
-                        include plugin_dir_path(__FILE__) . 'parts/reports/open-click-trends.php';
-                    } else if ($reportType == 'frequency') {
-                        include plugin_dir_path(__FILE__) . 'parts/reports/frequency.php';
-                    } else if ($reportType == 'signup-to-purchase') {
-                        include plugin_dir_path(__FILE__) . 'parts/reports/signup-to-purchase.php';
-                    }
-                    ?>
+                        if ($reportType == 'home') {
+                            include plugin_dir_path(__FILE__) . 'parts/reports/reports-home.php';
+                        } else if ($reportType == 'open-click-trends') {
+                            include plugin_dir_path(__FILE__) . 'parts/reports/open-click-trends.php';
+                        } else if ($reportType == 'frequency') {
+                            include plugin_dir_path(__FILE__) . 'parts/reports/frequency.php';
+                        } else if ($reportType == 'signup-to-purchase') {
+                            include plugin_dir_path(__FILE__) . 'parts/reports/signup-to-purchase.php';
+                        }
+                ?>
                 </div>
             </div>
 
