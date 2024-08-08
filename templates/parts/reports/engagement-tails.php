b8439d5e05ca3fed287cd6ec77c4ed34f81b4235
Eckstein <zac@zaceckstein.com>
2024-08-08 10:33:56 -0700

    adjust click threshold

--- templates/parts/reports/engagement-tails.php
+++ templates/parts/reports/engagement-tails.php
@@ -1,19 +1,19 @@
 <?php
 $fy24Campaigns = get_idwiz_campaigns(['startAt_start' => $startDate, 'startAt_end' => $endDate, 'type' => ['Blast']]);
 $maxHours = $_GET['maxhours'] ?? 72;
-$threshold = $_GET['threshold'] ?? 10;
+$openThreshold = $_GET['threshold'] ?? 10;
+$clickThreshold = $openThreshold * .1;
 
-$byHourAttsString = 
-'data-startdate="' . $startDate . 
-'" data-enddate="' . $endDate .
-'" data-maxhours="' . $maxHours . 
-'" data-threshold="' . $threshold .'"';
+$byHourAttsString =
+    'data-startdate="' . $startDate .
+    '" data-enddate="' . $endDate .
+    '" data-maxhours="' . $maxHours . '"';
 ?>
 <div class="wizChartWrapper">
-    <canvas id="opensByHourChart" class="engagementByHourChart" data-chartid="engagementByHour" data-campaignids='<?php echo json_encode(array_column($fy24Campaigns, 'id')); ?>' <?php echo $byHourAttsString; 
-                                                                                                                                                                                    ?>></canvas>
+    <canvas id="opensByHourChart" class="engagementByHourChart" data-chartid="engagementByHour" data-threshold="<?php echo $openThreshold; ?>" data-campaignids='<?php echo json_encode(array_column($fy24Campaigns, 'id')); ?>' <?php echo $byHourAttsString;
+                                                                                                                                                                                                                                    ?>></canvas>
 </div>
 <div class="wizChartWrapper">
-    <canvas id="clicksByHourChart" class="engagementByHourChart" data-chartid="engagementByHour" data-campaignids='<?php echo json_encode(array_column($fy24Campaigns, 'id')); ?>' <?php echo $byHourAttsString; 
-                                                                                                                                                                                    ?>></canvas>
+    <canvas id="clicksByHourChart" class="engagementByHourChart" data-chartid="engagementByHour" data-threshold="<?php echo $clickThreshold; ?>" data-campaignids='<?php echo json_encode(array_column($fy24Campaigns, 'id')); ?>' <?php echo $byHourAttsString;
+                                                                                                                                                                                                                                    ?>></canvas>
 </div>
