313ca1f8e4d1e3b07d96ab98a5e02f18580bfece
Eckstein <zac@zaceckstein.com>
2024-08-08 10:28:39 -0700

    Add filters to byHour charts

--- templates/parts/reports/engagement-tails.php
+++ templates/parts/reports/engagement-tails.php
@@ -1,7 +1,13 @@
 <?php
 $fy24Campaigns = get_idwiz_campaigns(['startAt_start' => $startDate, 'startAt_end' => $endDate, 'type' => ['Blast']]);
+$maxHours = $_GET['maxhours'] ?? 72;
+$threshold = $_GET['threshold'] ?? 10;
 
-$byHourAttsString = 'data-startdate="' . $startDate . '" data-enddate="' . $endDate . '" data-maxhours="72" data-threshold="10"';
+$byHourAttsString = 
+'data-startdate="' . $startDate . 
+'" data-enddate="' . $endDate .
+'" data-maxhours="' . $maxHours . 
+'" data-threshold="' . $threshold .'"';
 ?>
 <div class="wizChartWrapper">
     <canvas id="opensByHourChart" class="engagementByHourChart" data-chartid="engagementByHour" data-campaignids='<?php echo json_encode(array_column($fy24Campaigns, 'id')); ?>' <?php echo $byHourAttsString; 
--- templates/parts/reports/reports-filter-fields.php
+++ templates/parts/reports/reports-filter-fields.php
@@ -1,5 +1,7 @@
 <div id="wiz-report-filterbar" class="wizcampaign-sections-row">
-    <form id="reports-filter-form" class="<?php if ($reportType != 'open-click-trends'){echo 'refresh-on-submit';}; ?>">
+    <form id="reports-filter-form" class="<?php if ($reportType != 'open-click-trends') {
+                                                echo 'refresh-on-submit';
+                                            }; ?>">
         <?php
         $cohortLabels = get_all_cohort_labels();
 
@@ -21,72 +23,86 @@
 
         <?php if ($reportType == 'open-click-trends') { ?>
 
-        <fieldset class="wiz-report-controlset" id="wiz-report-sendsize-controls">
-            <div class="wiz-report-controlset-field">
-                <?php $setMin = isset($_GET['minSendSize']) ? $_GET['minSendSize'] : 1; ?>
-                <label for="wiz-report-sendsize-min">Min sends</label><input name="minSendSize" type="number" min="1" step="1" value="<?php echo $setMin; ?>" id="wiz-report-sendsize-min" />
-            </div>
-            <div class="wiz-report-controlset-field">
-                <?php $setMax = isset($_GET['maxSendSize']) ? $_GET['maxSendSize'] : 500000; ?>
-                <label for="wiz-report-sendsize-max">Max sends</label><input name="maxSendSize" type="number" min="1" step="1" value="<?php echo $setMax; ?>" id="wiz-report-sendsize-max" />
-            </div>
-        </fieldset>
-        <fieldset class="wiz-report-controlset" id="wiz-report-openrate-controls">
-            <div class="wiz-report-controlset-field">
-                <?php $setMinOpenRate = isset($_GET['minOpenRate']) ? $_GET['minOpenRate'] : 0; ?>
-                <label for="wiz-report-openrate-min">Min open rate</label><input name="minOpenRate" type="number" min="0" step="1" value="<?php echo $setMinOpenRate; ?>" id="wiz-report-openrate-min" />
-            </div>
-            <div class="wiz-report-controlset-field">
-                <?php $setMaxOpenRate = isset($_GET['maxOpenRate']) ? $_GET['maxOpenRate'] : 100; ?>
-                <label for="wiz-report-openrate-max">Max open rate</label><input name="maxOpenRate" type="number" min="0" step="0.01" value="<?php echo $setMaxOpenRate; ?>" id="wiz-report-openrate-max" />
-            </div>
-        </fieldset>
-        <fieldset class="wiz-report-controlset" id="wiz-report-clickrate-controls">
-            <div class="wiz-report-controlset-field">
-                <?php $setMinClickRate = isset($_GET['minClickRate']) ? $_GET['minClickRate'] : 0; ?>
-                <label for="wiz-report-clickrate-min">Min click rate</label><input name="minClickRate" type="number" min="0" step="0.01" value="<?php echo $setMinClickRate; ?>" id="wiz-report-clickrate-min" />
-            </div>
-            <div class="wiz-report-controlset-field">
-                <?php $setMaxClickRate = isset($_GET['maxClickRate']) ? $_GET['maxClickRate'] : 2; ?>
-                <label for="wiz-report-clickrate-max">Max click rate</label><input name="maxClickRate" type="number" min="0" step="0.01" value="<?php echo $setMaxClickRate; ?>" id="wiz-report-clickrate-max" />
-            </div>
-        </fieldset>
-        <fieldset class="wiz-report-controlset" id="wiz-report-ctorate-controls">
-            <div class="wiz-report-controlset-field">
-                <?php $setMinCtoRate = isset($_GET['minCtoRate']) ? $_GET['minCtoRate'] : 0; ?>
-                <label for="wiz-report-ctorate-min">Min CTO</label><input name="minCtoRate" type="number" min="0" step="0.01" value="<?php echo $setMinCtoRate; ?>" id="wiz-report-ctorate-min" />
-            </div>
-            <div class="wiz-report-controlset-field">
-                <?php $setMaxCtoRate = isset($_GET['maxCtoRate']) ? $_GET['maxCtoRate'] : 4; ?>
-                <label for="wiz-report-ctorate-max">Max CTO</label><input name="maxCtoRate" type="number" min="0" step="0.01" value="<?php echo $setMaxCtoRate; ?>" id="wiz-report-ctorate-max" />
-            </div>
+            <fieldset class="wiz-report-controlset" id="wiz-report-sendsize-controls">
+                <div class="wiz-report-controlset-field">
+                    <?php $setMin = isset($_GET['minSendSize']) ? $_GET['minSendSize'] : 1; ?>
+                    <label for="wiz-report-sendsize-min">Min sends</label><input name="minSendSize" type="number" min="1" step="1" value="<?php echo $setMin; ?>" id="wiz-report-sendsize-min" />
+                </div>
+                <div class="wiz-report-controlset-field">
+                    <?php $setMax = isset($_GET['maxSendSize']) ? $_GET['maxSendSize'] : 500000; ?>
+                    <label for="wiz-report-sendsize-max">Max sends</label><input name="maxSendSize" type="number" min="1" step="1" value="<?php echo $setMax; ?>" id="wiz-report-sendsize-max" />
+                </div>
+            </fieldset>
+            <fieldset class="wiz-report-controlset" id="wiz-report-openrate-controls">
+                <div class="wiz-report-controlset-field">
+                    <?php $setMinOpenRate = isset($_GET['minOpenRate']) ? $_GET['minOpenRate'] : 0; ?>
+                    <label for="wiz-report-openrate-min">Min open rate</label><input name="minOpenRate" type="number" min="0" step="1" value="<?php echo $setMinOpenRate; ?>" id="wiz-report-openrate-min" />
+                </div>
+                <div class="wiz-report-controlset-field">
+                    <?php $setMaxOpenRate = isset($_GET['maxOpenRate']) ? $_GET['maxOpenRate'] : 100; ?>
+                    <label for="wiz-report-openrate-max">Max open rate</label><input name="maxOpenRate" type="number" min="0" step="0.01" value="<?php echo $setMaxOpenRate; ?>" id="wiz-report-openrate-max" />
+                </div>
+            </fieldset>
+            <fieldset class="wiz-report-controlset" id="wiz-report-clickrate-controls">
+                <div class="wiz-report-controlset-field">
+                    <?php $setMinClickRate = isset($_GET['minClickRate']) ? $_GET['minClickRate'] : 0; ?>
+                    <label for="wiz-report-clickrate-min">Min click rate</label><input name="minClickRate" type="number" min="0" step="0.01" value="<?php echo $setMinClickRate; ?>" id="wiz-report-clickrate-min" />
+                </div>
+                <div class="wiz-report-controlset-field">
+                    <?php $setMaxClickRate = isset($_GET['maxClickRate']) ? $_GET['maxClickRate'] : 2; ?>
+                    <label for="wiz-report-clickrate-max">Max click rate</label><input name="maxClickRate" type="number" min="0" step="0.01" value="<?php echo $setMaxClickRate; ?>" id="wiz-report-clickrate-max" />
+                </div>
+            </fieldset>
+            <fieldset class="wiz-report-controlset" id="wiz-report-ctorate-controls">
+                <div class="wiz-report-controlset-field">
+                    <?php $setMinCtoRate = isset($_GET['minCtoRate']) ? $_GET['minCtoRate'] : 0; ?>
+                    <label for="wiz-report-ctorate-min">Min CTO</label><input name="minCtoRate" type="number" min="0" step="0.01" value="<?php echo $setMinCtoRate; ?>" id="wiz-report-ctorate-min" />
+                </div>
+                <div class="wiz-report-controlset-field">
+                    <?php $setMaxCtoRate = isset($_GET['maxCtoRate']) ? $_GET['maxCtoRate'] : 4; ?>
+                    <label for="wiz-report-ctorate-max">Max CTO</label><input name="maxCtoRate" type="number" min="0" step="0.01" value="<?php echo $setMaxCtoRate; ?>" id="wiz-report-ctorate-max" />
+                </div>
 
-        </fieldset>
-        <fieldset class="wiz-report-controlset" id="wiz-report-cohort-controls">
-            <div class="wiz-report-controlset-field">
-                <label for="wiz-report-cohorts">Include:</label>
-                <select id="wiz-report-cohorts" name="cohorts" class="cohort-select" multiple>
-                    <option value="all">All</option>
-                    <?php
-                    foreach ($cohortLabels as $cohortLabel) {
-                        $cohortSelected = in_array($cohortLabel, $selectedCohorts) ? 'selected' : '';
-                        echo '<option value="' . htmlspecialchars($cohortLabel) . '" ' . $cohortSelected . '>' . htmlspecialchars($cohortLabel) . '</option>';
-                    }
-                    ?>
-                </select>
-            </div>
-            <div class="wiz-report-controlset-field">
-                <label for="wiz-report-exclude_cohorts">Exclude</label>
-                <select id="wiz-report-exclude_cohorts" name="exclude_cohorts" class="cohort-exclude" multiple>
-                    <?php
-                    foreach ($cohortLabels as $cohortLabel) {
-                        $excludedCohortSelected = in_array($cohortLabel, $excludedCohorts) ? 'selected' : '';
-                        echo '<option value="' . htmlspecialchars($cohortLabel) . '" ' . $excludedCohortSelected . '>' . htmlspecialchars($cohortLabel) . '</option>';
-                    }
-                    ?>
-                </select>
-            </div>
-        </fieldset>
+            </fieldset>
+
+            <fieldset class="wiz-report-controlset" id="wiz-report-cohort-controls">
+                <div class="wiz-report-controlset-field">
+                    <label for="wiz-report-cohorts">Include:</label>
+                    <select id="wiz-report-cohorts" name="cohorts" class="cohort-select" multiple>
+                        <option value="all">All</option>
+                        <?php
+                        foreach ($cohortLabels as $cohortLabel) {
+                            $cohortSelected = in_array($cohortLabel, $selectedCohorts) ? 'selected' : '';
+                            echo '<option value="' . htmlspecialchars($cohortLabel) . '" ' . $cohortSelected . '>' . htmlspecialchars($cohortLabel) . '</option>';
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="wiz-report-controlset-field">
+                    <label for="wiz-report-exclude_cohorts">Exclude</label>
+                    <select id="wiz-report-exclude_cohorts" name="exclude_cohorts" class="cohort-exclude" multiple>
+                        <?php
+                        foreach ($cohortLabels as $cohortLabel) {
+                            $excludedCohortSelected = in_array($cohortLabel, $excludedCohorts) ? 'selected' : '';
+                            echo '<option value="' . htmlspecialchars($cohortLabel) . '" ' . $excludedCohortSelected . '>' . htmlspecialchars($cohortLabel) . '</option>';
+                        }
+                        ?>
+                    </select>
+                </div>
+            </fieldset>
+        <?php } ?>
+        <?php if ($_GET['reportType'] && $_GET['reportType'] == 'engagement-tails') { ?>
+            <fieldset class="wiz-report-controlset" id="wiz-report-maxhours-controls">
+                <div class="wiz-report-controlset-field">
+                    <?php $setMaxhours = isset($_GET['maxhours']) ? $_GET['maxhours'] : 72; ?>
+                    <label for="wiz-report-maxhours">Max Hours</label><input name="maxhours" type="number" min="0" step="1" value="<?php echo $setMaxhours; ?>" id="wiz-report-maxhours" />
+                </div>
+                <div class="wiz-report-controlset-field">
+                    <?php $setThreshold = isset($_GET['threshold']) ? $_GET['threshold'] : 50; ?>
+                    <label for="wiz-report-threshold">Threshold</label><input name="threshold" type="number" min="0" step="1" value="<?php echo $setThreshold; ?>" id="wiz-report-threshold" />
+                </div>
+
+            </fieldset>
         <?php } ?>
         <fieldset class="wiz-report-controlset" id="wiz-report-ctorate-controls">
             <div class="wiz-report-controlset-field">
