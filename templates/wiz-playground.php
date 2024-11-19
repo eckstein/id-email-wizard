c21bf7a9f2bbdd596154f8d24c5ca7f47fa2a2e7
Eckstein <zac@zaceckstein.com>
2024-11-19 09:30:31 -0800

    Change "price" to "total"

--- templates/wiz-playground.php
+++ templates/wiz-playground.php
@@ -9,21 +9,44 @@ global $wpdb;
 		<div class="wizHeaderInnerWrap">
 			<div class="wizHeader-left">
 				<h1 class="wizEntry-title" itemprop="name">
-					Playground
+					Online Private Lessons Purchase Analysis
 				</h1>
-			</div>
-			<div class="wizHeader-right">
-				<div class="wizHeader-actions">
-
-				</div>
 			</div>
 		</div>
 	</header>
 	<div class="entry-content" itemprop="mainContentOfPage">
+		<?php
+		$query = "
+			SELECT 
+				p.purchaseDate,
+				u.signupDate
+			FROM {$wpdb->prefix}idemailwiz_purchases p
+			JOIN {$wpdb->prefix}idemailwiz_users u 
+				ON p.userId = u.userId
+			WHERE p.shoppingCartItems_divisionName = 'Online Private Lessons'
+			ORDER BY p.purchaseDate DESC
+			LIMIT 10000
+		";
 		
-			
+		$results = $wpdb->get_results($query);
+		?>
 		
-	</div>
+		<table class="idemailwiz_table display">
+			<thead>
+				<tr>
+					<th>Purchase Date</th>
+					<th>Signup Date</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach ($results as $row): ?>
+					<tr>
+						<td><?php echo date('Y-m-d', strtotime($row->purchaseDate)); ?></td>
+						<td><?php echo date('Y-m-d', strtotime($row->signupDate)); ?></td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
 	</div>
 </article>
 
